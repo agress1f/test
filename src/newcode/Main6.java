1546c2c0cb5f3c22cc623e6dabc6bf0b07ae2ff5
Agress1f <945797909@qq.com>
2020-04-01 00:03:07 +0800

    4/1

---
+++ src/newcode/Main6.java
@@ -0,0 +1,30 @@
+package newcode;
+
+public class Main6 {
+//    public int minNumberInRotateArray(int [] array) {
+//        for(int i=1;i<array.length;i++){
+//            if(array[i-1]>array[i])
+//                return array[i];
+//        }
+//        return array[0];
+
+    //二分法
+    public static int minNumberInRotateArray(int [] array){
+        int l = 0;
+        int r = array.length-1;
+        int mid = (l+r)>>1;
+        while (array[l]<array[r-1]){
+            if (array[mid]>=array[l]){
+                l=mid;
+            }else if (array[mid]<=array[r]){
+                r=mid;
+            }
+        }
+        return array[r];
+    }
+
+    public static void main(String arg[]){
+        int[] array = {3,4,5,1,2};
+        System.out.println(minNumberInRotateArray(array));
+    }
+}
